6ea571f783f065bfc33779f237d992e445dcb9dc
Christopher Guess <cguess@gmail.com>
2022-04-19 22:05:05 -0400

    rename filename to file_name and foldername to folder_name

--- lib/eikon/video_processor.rb
+++ lib/eikon/video_processor.rb
@@ -7,41 +7,44 @@ module Eikon
     extend T::Sig
 
     sig { returns(String) }
-    attr_reader :filename
+    attr_reader :file_name
 
-    sig { params(filename: String).void }
-    def initialize(filename)
-      @filename = T.let(filename, String)
+    sig { params(file_name: String).void }
+    def initialize(file_name)
+      @file_name = T.let(file_name, String)
     end
 
     sig { returns(String) }
     def split_video_into_images
-      filename = get_file_name(@filename)
+      file_name = get_file_name(@file_name)
 
       setup_tmp_folder
 
-      output_folder_path = "tmp/videos/#{filename}_#{Time.now.utc.strftime("%Y%m%d%H%M%S")}"
+      output_folder_path = "tmp/videos/#{file_name}_#{Time.now.utc.strftime("%Y%m%d%H%M%S")}"
       begin
-        make_dir_command = Terrapin::CommandLine.new("mkdir", ":foldername")
-        make_dir_command.run(foldername: output_folder_path)
+        make_dir_command = Terrapin::CommandLine.new("mkdir", ":folder_name")
+        make_dir_command.run(folder_name: output_folder_path)
       rescue Terrapin::ExitStatusError
         raise "Folder already exists for this export"
       end
 
       # ffmpeg -i pexels-ron-lach-7121125.mp4 -vf fps=10/60 out%d.png
-      line = Terrapin::CommandLine.new("ffmpeg", "-i :filename -vf fps=10/60 :foldername")
-      line.run(filename: @filename, foldername: "#{output_folder_path}/#{filename}_%d.png")
+      line = Terrapin::CommandLine.new("ffmpeg", "-i :file_name -vf fps=10/60 :folder_name")
+      line.run(file_name: @file_name, folder_name: "#{output_folder_path}/#{file_name}_%d.png")
       output_folder_path
+    end
+
+    def get_frames_dhash(folder_path)
     end
 
   private
 
-    sig { params(filename: String).returns(String) }
-    def get_file_name(filename)
-      filename_parts = filename.split(".")
-      filename_parts.select! { |e| !e.empty? }
-      filename = filename_parts.first
-      T.must(T.must(filename).split("/").last)
+    sig { params(file_name: String).returns(String) }
+    def get_file_name(file_name)
+      file_name_parts = file_name.split(".")
+      file_name_parts.select! { |e| !e.empty? }
+      file_name = file_name_parts.first
+      T.must(T.must(file_name).split("/").last)
     end
 
     sig { void }
